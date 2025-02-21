7e00aada0ea8887545a19d3e949d1cb4bed1d6fd
zcemua0 <you@example.com>
2025-02-21 21:44:17 +0000

    Excercise 1

--- src/student/dash_single/paralympics_dash.py
+++ src/student/dash_single/paralympics_dash.py
@@ -4,12 +4,23 @@ from dash import Dash, html
 # Create an instance of the Dash app
 app = Dash(__name__)
 
-# Add an HTML layout to the Dash app
+# Add an HTML layout to the Dash app (Using HTML in Dash)
+# This is the HTML to create page layout
 app.layout = html.Div([
-    # Add an HTML div with the text 'Hello World'
-    html.Div(children='Hello World')
+    # children is a special attribute and if used as the first attribute does not need to be named
+    # create the heading
+    html.H1(children='Hello Dash'),  # similar to html.H1('Hello Dash')
+    # Add an HTML div contains html.H1, html.P, html.Img
+    # Div = division, use to create sections/divisions
+    html.Div(children=[
+        html.H1('A First Heading'),
+        html.P('Dash: A web application framework for Python.'),  # paragraph
+        # This line has been split to adhere to the line length limit
+
+        html.Img(src=app.get_asset_url('bar-chart-placeholder.png'))
+    ])
 ])
 
 # Run the app
 if __name__ == '__main__':
-    app.run(debug=True, port=5050)
+    app.run(debug=True, port=5051)
