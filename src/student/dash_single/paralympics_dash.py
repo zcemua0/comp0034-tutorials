b09c6df0276f09a1812e16f87c6119f24262765f
zcemua0 <you@example.com>
2025-02-21 22:45:00 +0000

    week 1 activity 4

--- src/student/dash_single/paralympics_dash.py
+++ src/student/dash_single/paralympics_dash.py
@@ -1,14 +1,23 @@
 # Imports for Dash and Dash.html
+# Import Dash Bootstrap Components (dbc)
 from dash import Dash, html
+import dash_bootstrap_components as dbc
 
-# Create an instance of the Dash app
-app = Dash(__name__)
+# Variable that defines the meta tag for the viewport (required to support responsive design)
+meta_tags = [
+    {"name": "viewport", "content": "width=device-width, initial-scale=1"},
+]
+
+# Variable that contains the external_stylesheet to use, in this case Bootstrap styling from dash bootstrap components (dbc)
+external_stylesheets = [dbc.themes.BOOTSTRAP]
+
+# Pass the stylesheet and meta_tag variables to the Dash app constructor
+app = Dash(__name__, external_stylesheets=external_stylesheets, meta_tags=meta_tags)
 
 # Add an HTML layout to the Dash app (Using HTML in Dash)
 # This is the HTML to create page layout
-app.layout = html.Div([
-    # children is a special attribute and if used as the first attribute does not need to be named
-    # create the heading
+app.layout = dbc.Container([
+    # Heading
     html.H1(children='Hello Dash'),  # similar to html.H1('Hello Dash')
     # Add an HTML div contains html.H1, html.P, html.Img
     # Div = division, use to create sections/divisions
@@ -23,4 +32,4 @@ app.layout = html.Div([
 
 # Run the app
 if __name__ == '__main__':
-    app.run(debug=True, port=5051)
+    app.run(debug=True, port=5050)
