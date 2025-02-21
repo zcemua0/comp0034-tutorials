984d19f221e348b9f5147847609f844a4207f8e7
zcemua0 <you@example.com>
2025-02-21 23:44:36 +0000

    adding dark theme toggle

--- src/student/dash_single/paralympics_dash.py
+++ src/student/dash_single/paralympics_dash.py
@@ -1,34 +1,58 @@
-# Imports for Dash and Dash.html
-# Import Dash Bootstrap Components (dbc)
-from dash import Dash, html
+from dash import Dash, html, Input, Output, clientside_callback
 import dash_bootstrap_components as dbc
 
-# Variable that defines the meta tag for the viewport (required to support responsive design)
+# Define the meta tag for the viewport (required to support responsive design)
 meta_tags = [
     {"name": "viewport", "content": "width=device-width, initial-scale=1"},
 ]
 
-# Variable that contains the external_stylesheet to use, in this case Bootstrap styling from dash bootstrap components (dbc)
-external_stylesheets = [dbc.themes.BOOTSTRAP]
+# Variable that contains the external_stylesheet to use, 
+external_stylesheets = [dbc.themes.BOOTSTRAP, dbc.icons.FONT_AWESOME]
 
 # Pass the stylesheet and meta_tag variables to the Dash app constructor
 app = Dash(__name__, external_stylesheets=external_stylesheets, meta_tags=meta_tags)
 
-# Add an HTML layout to the Dash app (Using HTML in Dash)
+# colormode switch
+color_mode_switch = html.Span([
+    dbc.Label(className="fa fa-moon", html_for="theme-switch"),
+    dbc.Switch(id="theme-switch", value=False, className="d-inline-block ms-1", persistence=True),
+    dbc.Label(className="fa fa-sun", html_for="theme-switch"),
+])
+
 # This is the HTML to create page layout
 app.layout = dbc.Container([
-    # Heading
-    html.H1(children='Hello Dash'),  # similar to html.H1('Hello Dash')
-    # Add an HTML div contains html.H1, html.P, html.Img
-    # Div = division, use to create sections/divisions
-    html.Div(children=[
-        html.H1('A First Heading'),
-        html.P('Dash: A web application framework for Python.'),  # paragraph
-        # This line has been split to adhere to the line length limit
-
+    html.H1(children='Hello Dash'), 
+     
+    # Add theme switcher
+    html.Div(children=[ 
+        dbc.Switch(
+            id='theme-switch',
+            value=False,
+            className="d-inline-block ms-1",
+            persistence=True,
+            label='Theme Mode',
+            style={'margin': '20px'}
+        ),
+        html.H1('A First Heading'),  
+        html.P('Toggle the theme using the switch below:'),
+        html.P('Dash: A web application framework for Python.'),        
         html.Img(src=app.get_asset_url('bar-chart-placeholder.png'))
-    ])
+    ]),
+    # Hidden div for setting Bootstrap theme
+    html.Div(id="theme-container", style={"display": "none"})
 ])
+
+# Client-side callback to toggle theme
+clientside_callback(
+    """
+    (switchOn) => {
+        document.documentElement.setAttribute("data-bs-theme", switchOn ? "dark" : "light");
+        return window.dash_clientside.no_update;
+    }
+    """,
+    Output("theme-switch", "id"), 
+    Input("theme-switch", "value"),
+)
 
 # Run the app
 if __name__ == '__main__':
